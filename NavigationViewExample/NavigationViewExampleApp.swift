2f27889f2f8eeaf8aee48e7c31f3328f100f376f
Frederick Javalera <frederickjavalera@gmail.com>
2021-05-23 13:47:03 -0600

    Initial Commit

---
+++ NavigationViewExample/NavigationViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewExampleApp.swift
+//  NavigationViewExample
+//
+//  Created by Frederick Javalera on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
